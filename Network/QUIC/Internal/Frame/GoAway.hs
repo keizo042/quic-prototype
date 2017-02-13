b2693dea91132eb7f162c3aa672c77c922e3cf87
Kouichi Nakanishi <keizo.bookman@gmail.com>
2017-02-14 00:36:21 +0900

    Progress

--- Network/QUIC/Internal/Frame/GoAway.hs
+++ Network/QUIC/Internal/Frame/GoAway.hs
@@ -1,32 +1,35 @@
-module Network.QUIC.Internal.Frame.GoAway where
+module Network.QUIC.Internal.Frame.GoAway 
+  (
+  )where
 import Network.QUIC.Error
+import Network.QUIC.Types
+import Network.QUIC.Internal.Util.Binary
+
 import Data.Word
 import Data.Binary.Get
 import Data.Binary.Put
-import Data.ByteString.Lazy
+import qualified Data.ByteString.Lazy as BSL
+import qualified Data.ByteString as BS
 
-data GoAwayFrame = GoAwayFrame { goAwayErrorCode        :: ErrorCodes
-                               , goAwayLastGoodStreamId :: Int
-                               , goAwayReasonPhase      :: ByteString
+data GoAwayFrame = GoAwayFrame { goAwayErrorCode        :: !ErrorCodes
+                               , goAwayLastGoodStreamId :: !StreamID
+                               , goAwayReasonPhase      :: !BS.ByteString
                                } deriving Show
                               
 
 
-decodeGoAwayFrame ::  ByteString -> QUICResult (GoAwayFrame, ByteString)
-decodeGoAwayFrame bs =  case BG.runGetOrFail get bs of
+decodeGoAwayFrame ::  BSL.ByteString -> ByteSize -> QUICResult (GoAwayFrame, BSL.ByteString)
+decodeGoAwayFrame bs n =  case runGetOrFail get bs of
                           Right (bs, _, frame) -> Right (frame, bs)
-                          Left _ -> Left Error.InvalidGoAwayData
+                          Left _ -> Left InvalidGoAwayData
   where 
-    get :: BG.Get Frame
-    get = GoAwayFrame <$> err <*> getInt32 <*> reason
-      where
-        err :: BG.Get ErrorCodes
-        err = Error.int2err <$> getInt32
-        reason :: Get ByteString
-        reason = fromIntegral <$> getInt16 >>= getLazyByteString
+    get :: Get GoAwayFrame
+    get = GoAwayFrame <$> getErrorCode <*> getStreamID n  <*> getReasonPhase
 
-
-encodeGoAwayFrame :: GoAwayFrame -> ByteString
-encodeGoAwayFrame = runPut put
+encodeGoAwayFrame :: GoAwayFrame -> BSL.ByteString
+encodeGoAwayFrame (GoAwayFrame errCode streamID reasonPhase) = runPut put
   where
-    put = undefined
+    put = do 
+      putErrorCode errCode 
+      putStreamID (countStreamIDbyteSize streamID) streamID
+      putReasonPhase reasonPhase
